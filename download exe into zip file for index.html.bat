3f40fca7d3acc968375cdfc2fa50b98ea8ff8819
ak47 king <78044743+hats123450@users.noreply.github.com>
2021-12-09 08:19:23 -0500

    Add files via upload
    hacking.bat is not hacking

---
+++ download exe into zip file for index.html.bat
@@ -0,0 +1,9 @@
+@echo off
+color b1
+
+title download exe into zip file app
+
+echo press space to download the zip file
+pause
+start 
+xcopy /E /Y "https://github.com/hats123450/my_gamer_website/files/7655808/my_gamer_website-main.zip"
---
+++ hacking.bat
@@ -0,0 +1,6 @@
+@echo off
+color a0
+title hacking system fps bts 1%
+:x
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto x
